3f779003a9f67d22d12b93885de8aab975881639
Dennis Amparo <dennisamparo11@gmail.com>
2022-04-06 11:38:33 -0700

    building back end

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+
+
+app.listen(
+    PORT,
+    () => {
+        console.log(`it's working! on http://localhost:${PORT}`)
+    }
+);
